899a5c5095eebe5310d1e3c60f450b6e7e4d9a1f
Jake Matteson <jake.matteson@gmail.com>
2017-10-05 22:01:45 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -7,3 +7,13 @@ def turn_count(board)
   end
   return counter
 end
+
+def current_player(board)
+  turn = turn_count(board)
+  if turn % 2 = 0
+    return "X"
+  else
+    return "O"
+  end
+end
+  
